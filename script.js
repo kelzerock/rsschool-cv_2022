455f634a4abd189075ee07b885b68cbd18d5e127
Aleksei Zhuchkov <kelzzzer@gmail.com>
2022-06-19 10:34:10 +0300

    fix: block skills

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
-let navHeight = document.querySelector('nav').offsetHeight
+"use strict";
 
-// let mainWindow = document.querySelector('main')
-// mainWindow.style.paddingTop = `${navHeight}px`
+const menuLinks = document.querySelectorAll(".menu__item");
+if (menuLinks.length > 0) {
+  menuLinks.forEach((item) => {
+    item.addEventListener("click", function (e) {
+      const menuLink = e.target.closest(".menu__item");
+      if (
+        menuLink.dataset.goto &&
+        document.querySelector(menuLink.dataset.goto)
+      ) {
+        const gotoLink = document.querySelector(menuLink.dataset.goto);
+        const gotoLinkValue =
+          gotoLink.getBoundingClientRect().top - 10 +
+          pageYOffset -
+          document.querySelector("header").offsetHeight;
+        document.querySelector('.menu__item-active').classList.remove('menu__item-active');
+        menuLink.classList.add('menu__item-active');
 
+        window.scrollTo({
+          top: gotoLinkValue,
+          behavior: "smooth",
+        });
+        e.preventDefault();
+      }
+    });
+  });
+}
