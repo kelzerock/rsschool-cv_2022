3a79030d2ef3d326e21aabaa1d4b698d8375eeb6
Aleksei Zhuchkov <kelzzzer@gmail.com>
2022-06-19 12:31:27 +0300

    add: burger menu

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 "use strict";
 
+//menu burger
+const iconMenu = document.querySelector('.menu_icon')
+const menuBody = document.querySelector('.menu')
+if(iconMenu){
+  iconMenu.addEventListener('click', function(e){
+    document.body.classList.toggle('_lock')
+    iconMenu.classList.toggle('_active')
+    menuBody.classList.toggle('_active')
+  })
+}
+
 const menuLinks = document.querySelectorAll(".menu__item");
 if (menuLinks.length > 0) {
   menuLinks.forEach((item) => {
@@ -16,6 +27,12 @@ if (menuLinks.length > 0) {
           document.querySelector("header").offsetHeight;
         document.querySelector('.menu__item-active').classList.remove('menu__item-active');
         menuLink.classList.add('menu__item-active');
+
+        if(iconMenu.classList.contains('_active')){
+          document.body.classList.remove('_lock')
+          iconMenu.classList.remove('_active')
+          menuBody.classList.remove('_active')
+        }
 
         window.scrollTo({
           top: gotoLinkValue,
